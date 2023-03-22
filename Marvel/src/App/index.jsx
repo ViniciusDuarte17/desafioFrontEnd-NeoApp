3326eb0a5dd3caf97f44760c7c76acf9a6c9886f
ViniciusDuarte17 <vinicius.benduarte14@gmail.com>
2023-03-22 09:56:07 -0300

    feat: Estrutura inicial/Configurando estilo global

---
+++ Marvel/src/App/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import GlobalStyled from "../GlobalStyled";
+
+
+const App = () => {
+
+  return (
+    <>
+      <GlobalStyled />
+    </>
+  )
+}
+
+export default App
---
+++ Marvel/src/GlobalStyled/index.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+
+* {
+    margin: 0;
+    padding: 0;
+    font-family: 'Nunito', sans-serif;
+    box-sizing:  border-box;
+}
+ 
+body {
+        @import url('https://fonts.googleapis.com/css?family=Montserrat:400,600&display=swap');
+        font-family: 'Montserrat', sans-serif;
+        margin: 0;
+        padding: 0;
+        background-color: #fff;
+    }
+
+    input, button, textarea, h1, h2, h3, h4, h5, h6 {
+        font-family: 'Nunito', sans-serif;
+    }
+`;
